1b00264b8d9e6cfbdd8fd8cde3f282ed4ee1a2ac
justice jennings <justice.l.jennings@gmail.com>
2023-11-21 15:17:36 -0500

    added last step.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import Blue from './components/Blue';
 import Red from './components/Red';
 import Home from './components/Home';
+import './index.css';
 
 
 
