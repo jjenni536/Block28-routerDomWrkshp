67637af9b2ec47efc0094a3790a1c3dbd840533d
justice jennings <justice.l.jennings@gmail.com>
2023-11-21 13:13:48 -0500

    added home component link and imported to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Routes, Route, Link } from "react-router-dom";
 import Blue from './components/Blue';
 import Red from './components/Red';
+import Home from './components/Home';
 
 
 
@@ -16,11 +17,13 @@ function App() {
         <div id="navbar">
           <Link to= '/blue'>Blue</Link>
           <Link to= '/red'>Red</Link>
+          <Link to= '/'>Home</Link>
         </div>
         <div id="main-section">
           <Routes>
             <Route path='/blue' element={<Blue/>} />
             <Route path='/red' element= {<Red/>} />
+            <Route path='/' element= {<Home/>} />
           </Routes>
         </div>
 
