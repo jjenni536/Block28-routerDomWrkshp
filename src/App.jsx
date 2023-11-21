21b578e2077ff5e5cfdcbf5cde144a685d73537f
justice jennings <justice.l.jennings@gmail.com>
2023-11-21 12:49:25 -0500

    added nav links and are displayed on screen.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 
 
 
@@ -11,11 +11,14 @@ function App() {
 
       <div id="container">
         <h1></h1>
-        <div id="navbar">{/* navigation here */}</div>
+        <div id="navbar">
+          <Link to= '/blue'>Blue</Link>
+          <Link to= '/red'>Red</Link>
+        </div>
         <div id="main-section">
           <Routes>
             <Route path='/blue' element={<h1>Blue</h1>} />
-            <Route path='/red' element={<h1>Blue</h1>} />
+            <Route path='/red' element={<h1>Red</h1>} />
           </Routes>
         </div>
 
